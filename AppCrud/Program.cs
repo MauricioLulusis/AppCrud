e48b67e39047f8893324299105c7f5b9bfb075c6
Mauricio Lulusis <mauricio.lulusis@intuit.ar>
2024-04-22 15:00:10 -0300

    Configuración de la tabla Empleado, configuración de la cadena de conexión de SQL, creación de las migrations

--- AppCrud/Program.cs
+++ AppCrud/Program.cs
@@ -1,7 +1,16 @@
+using AppCrud.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
 
 var app = builder.Build();
 
